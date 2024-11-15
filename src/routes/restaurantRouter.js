2b5d98e718e1bd2c7b20d3d43ae3f0ca7469c990
SUNAD K <2023mt93011@wilp.bits-pilani.ac.in>
2024-11-16 01:03:31 +0530

    adding register and login

--- src/routes/restaurantRouter.js
+++ src/routes/restaurantRouter.js
@@ -24,10 +24,29 @@ function isAdminOrRestaurant (userId) {
 
 restaurantRouter.post("/register", async (req, res) => {
     try {
-        const { userId, restaurantName, address, cuisineType, openingHours, deliveryZone } = req.body;
-        isAdminOrRestaurant(userId);
+        // Validation of data
+        validateSignUpData(req);
+        const { firstName, lastName, emailId, password, role, gender, restaurantName, address, cuisineType, openingHours, deliveryZone } = req.body;
+        if (role !== "restaurant") {
+            throw new Error("Invalid role");
+        }
+
+        // Encrypt the password
+        const passwordHash = await bcrypt.hash(password, 10);
+        console.log(passwordHash);
+
+        //   Creating a new instance of the User model
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash,
+            role,
+            gender
+        });
+        const savedUser = await user.save();
         const restaurant = new Restaurant({
-            owner: userId,
+            owner: savedUser._id,
             restaurantName,
             address,
             cuisineType,
@@ -35,11 +54,45 @@ restaurantRouter.post("/register", async (req, res) => {
             deliveryZone
         })
         const savedRestaurant = await restaurant.save()
+        // const savedUser = await user.save();
         const token = await savedUser.getJWT();
         res.cookie("token", token, {
             expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
         });
-        res.json({ message: "Customer Data added successfully!", data: savedRestaurant});
+
+        const response = { user: savedUser, restaurant: savedRestaurant};
+        res.json({ message: "Customer Data added successfully!", data: response});
+    } catch (err) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+});
+
+restaurantRouter.post("/login", async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+
+        const user = await User.findOne({ emailId: emailId });
+        if (!user) {
+            throw new Error("Invalid credentials");
+        }
+        if (user.role !== "restaurant") {
+            throw new Error("Invalid role");
+        }
+        if (user.status !== "active") {
+            throw new Error("Customer account is not active");
+        }
+        const isPasswordValid = await user.validatePassword(password);
+
+        if (isPasswordValid) {
+            const token = await user.getJWT();
+
+            res.cookie("token", token, {
+                expires: new Date(Date.now() + 8 * 3600000),
+            });
+            res.send(user);
+        } else {
+            throw new Error("Invalid credentials");
+        }
     } catch (err) {
         res.status(400).send("ERROR : " + err.message);
     }
