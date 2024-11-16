32ff82c7a2215aee4104b87f68a96bca8f70a097
Immuu19 <mohammedmrn7@gmail.com>
2024-11-16 20:35:26 +0530

    Update generateToken.js

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,7 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_PRIVATE_KEY, { expiresIn: '30d' });
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      userId: user._id,
+      tokenVersion: user.tokenVersion,
+    },
+    process.env.JWT_PRIVATE_KEY,
+    { expiresIn: '10m' }
+  );
 };
-
 module.exports = generateToken;
