a37d53b5a562ac481b8c0bae16b4bcf99eaf5d0b
Immuu19 <mohammedmrn7@gmail.com>
2024-11-16 20:34:01 +0530

    Update customerRouter.js

--- src/routes/customerRouter.js
+++ src/routes/customerRouter.js
@@ -110,6 +110,97 @@ customerRouter.post("/logout", userAuth, async (req, res) => {
     res.send("Logout is successfull!!");
 });
 
+const OrderItem = require('../models/OrderItem');
+const Restaurant = require('../models/Restaurant');
+const Menu = require('../models/Menu');
+const Order = require('../models/Order');
+
+customerRouter.post("/placeOrder", userAuth, async (req, res) => {
+  try {
+    const { customerId, restaurantId, items, deliveryTime } = req.body;
+
+    if (!customerId || !restaurantId || !items || items.length === 0) {
+      return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    const customer = await User.findById(customerId);
+    if (!customer) {
+      return res.status(404).json({ error: 'Customer not found' });
+    }
+
+    const restaurant = await Restaurant.findById(restaurantId);
+    if (!restaurant) {
+      return res.status(404).json({ error: 'Restaurant not found' });
+    }
+
+    let totalAmount = 0;
+    const orderItems = [];
+    
+
+    for (const item of items) {
+      const { menuItem, quantity } = item;
+      if (!menuItem || !quantity) {
+        return res.status(400).json({ error: 'Invalid item details' });
+      }
+      
+
+      const menuItemOut = await Menu.findById(menuItem);
+      if (!menuItemOut) {
+        return res.status(404).json({ error: `Menu item with ID ${menuItem} not found` });
+      }
+
+      
+
+      const itemTotal = menuItemOut.price * quantity;
+      totalAmount += itemTotal;
+      
+
+      const newOrderItem = new OrderItem({
+        menuItem: menuItemOut._id,
+        quantity,
+        order: null,
+      });
+      
+
+      orderItems.push(newOrderItem);
+    }
+
+    let deliveryDate = null;
+    if (deliveryTime) {
+      deliveryDate = new Date(deliveryTime);
+      if (isNaN(deliveryDate.getTime())) {
+        return res.status(400).json({ error: 'Invalid deliveryTime format' });
+      }
+    }
+
+    const newOrder = new Order({
+      customer: customerId,
+      restaurant: restaurantId,
+      items: [],
+      orderStatus: 'Pending',
+      totalAmount,
+      deliveryTime: deliveryDate,
+    });
+
+    const savedOrder = await newOrder.save();
+
+    for (const orderItem of orderItems) {
+      orderItem.order = savedOrder._id;
+      await orderItem.save();
+    }
+
+    savedOrder.items = orderItems.map(item => item._id);
+    await savedOrder.save();
+
+    res.status(201).json({
+      message: 'Order placed successfully',
+      order: savedOrder,
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create order' })
+    console.log(error);
+  }
+});
 
 
 module.exports = customerRouter;
